3be2f4150f62abcf3e1a780a218b2333a95dff94
DouglasAledi <douglasaledi2@hotmail.com>
2026-02-28 21:51:21 -0300

    Implementa API simples com GET e POST persistindo em JSON

--- codigo_sistema_de_produtos/main.go
+++ codigo_sistema_de_produtos/main.go
@@ -14,6 +14,7 @@ type Name struct {
 func main() {
 
 	http.HandleFunc("/nome", metodoPegar)
+	http.HandleFunc("/nome", metodoAdicionar)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -27,8 +28,8 @@ func metodoPegar(w http.ResponseWriter, r *http.Request) {
 	var names []Name
 
 	file, err := os.OpenFile("Nome.json", os.O_RDONLY|os.O_CREATE, 0644)
-	if err != nil {
-		http.Error(w, "Erro ao abrir arquivo", http.StatusInternalServerError)
+	if err != nil && err != io.EOF {
+		http.Error(w, "Erro ao ler JSON", http.StatusInternalServerError)
 		return
 	}
 
@@ -45,5 +46,53 @@ func metodoPegar(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(names)
+}
 
+func metodoAdicionar(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	file, err := os.OpenFile("Nome.json", os.O_RDWR|os.O_CREATE, 0644)
+
+	var names []Name
+
+	if err != nil && err != io.EOF {
+		http.Error(w, "Erro ao ler o arquivo", http.StatusInternalServerError)
+		return
+	}
+
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&names)
+
+	if err != nil {
+		http.Error(w, "Erro ao ler JSON", http.StatusInternalServerError)
+		return
+	}
+
+	var novo Name
+
+	err = json.NewDecoder(r.Body).Decode(&novo)
+
+	if err != nil {
+		http.Error(w, "Erro ao ler corpo", http.StatusBadRequest)
+		return
+	}
+
+	names = append(names, novo)
+
+	file.Truncate(0)
+	file.Seek(0, 0)
+
+	err = json.NewEncoder(file).Encode(names)
+
+	if err != nil {
+		http.Error(w, "Erro ao salvar JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
